d46b6ad163c7f9cead250263dd115df71a04f628
Dragan Arsic <draganarsic1999@hotmail.com>
2020-08-22 11:21:29 +0200

    dodat UrednikHomepage

---
+++ sims/src/view/UrednikHomepage.java
@@ -0,0 +1,19 @@
+package view;
+
+import model.Sesija;
+
+public class UrednikHomepage extends Homepage {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public UrednikHomepage(Sesija sesija) {
+        super(sesija);
+        // TODO Auto-generated constructor stub
+    }
+    
+    
+
+}
